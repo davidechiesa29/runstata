075af61e77633e68ee6421d5293baea2b0ccfa25
David Chiesa <davidechiesa29@gmail.com>
2026-05-17 20:59:21 -0400

    Created home page and navbar

---
+++ runstata/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import '../styles/home.css';
+
+function Home() {
+  return (
+      <div style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
+        <h1 id="logo"> RunStata </h1>
+        <div id="triangle"></div>
+      </div>
+
+  );
+};
+
+export default Home;
