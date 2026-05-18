0fe3cd6159efa493d5732ea055a5aa8c774d6934
Davide <davidechiesa29@gmail.com>
2026-05-18 00:24:23 -0400

    added get started button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import '../styles/home.css';
+import { FaArrowRight } from 'react-icons/fa';
 
 function Home() {
   return (
@@ -6,6 +7,9 @@ function Home() {
      <div style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
         <h1 id="logo"> RunStata </h1>
         <p id="catchline"> Track your runs. Analyze your stats. </p>
+        <button id="getstarted"> Get Started
+          <FaArrowRight style={{ marginLeft: "8px" }} />
+        </button>
       </div>
       <div id="triangleContainer">
         <div id="triangleLeft"></div>
