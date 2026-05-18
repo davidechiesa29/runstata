19282f4e876c23f27bf29b39d2731966b9bd6faa
David Chiesa <davidechiesa29@gmail.com>
2026-05-17 21:30:52 -0400

    Improved homepage and navbar

--- runstata/src/pages/Home.tsx
+++ runstata/src/pages/Home.tsx
@@ -2,10 +2,19 @@ import '../styles/home.css';
 
 function Home() {
   return (
-      <div style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
+    <>
+     <div style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
         <h1 id="logo"> RunStata </h1>
+        
+      </div>
+      <div id="triangleContainer">
+        <div id="triangleLeft"></div>
+        <div id="triangleRight"></div>
         <div id="triangle"></div>
       </div>
+      
+    </>
+     
 
   );
 };
